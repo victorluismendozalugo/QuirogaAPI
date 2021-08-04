888685daa18187475584e6498d4c4d4f86ddbd35
kragsoftware <kragsoftware@gmail.com>
2021-08-04 17:45:33 -0600

    opciones para la configuracion del menu

---
+++ apiQuiroga.Models/Configuracion/PerfilesUsuarioModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apiQuiroga.Models.Configuracion
+{
+    public class PerfilesUsuarioModel
+    {
+        public int CodigoPerfil { get; set; }
+        public string DescripcionPerfil { get; set; }
+        public string Estatus { get; set; }
+        public string AdicionadoPor { get; set; }
+        public string FechaAdicion { get; set; }
+        public string ModificadoPor { get; set; }
+        public string FechaModificacion { get; set; }
+
+        //opciones X perfil
+        public string DescripcionMenu { get; set; }
+        public string Tipo { get; set; }
+        public int CodigoPadre { get; set; }
+        public int Orden { get; set; }
+        public int CodigoMenuOpcion { get; set; }
+
+    }
+}
